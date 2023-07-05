e8193645c8c69f215f76ea0de05a5fdefb56954b
Violet-Scarelli <alphaone150@gmail.com>
2023-07-05 16:41:55 -0700

    registering the basic coins as a test

--- common/src/main/java/dev/ithundxr/createnumismatics/registry/NumismaticsItems.java
+++ common/src/main/java/dev/ithundxr/createnumismatics/registry/NumismaticsItems.java
@@ -11,6 +11,12 @@ public class NumismaticsItems {
 	private static final CreateRegistrate REGISTRATE = Numismatics.registrate();
 
 	public static final ItemEntry<Item> EXAMPLE_ITEM = REGISTRATE.item("example_item", Item::new).register();
+	public static final ItemEntry<Item> SPUR = REGISTRATE.item("spur", Item::new).lang("Spur").register();
+	public static final ItemEntry<Item> BEVEL = REGISTRATE.item("bevel", Item::new).lang("Bevel").register();
+	public static final ItemEntry<Item> SPROCKET = REGISTRATE.item("sprocket", Item::new).lang("Sprocket").register();
+	public static final ItemEntry<Item> COG = REGISTRATE.item("cog", Item::new).lang("Cog").register();
+	public static final ItemEntry<Item> CROWN = REGISTRATE.item("crown", Item::new).lang("Crown").register();
+	public static final ItemEntry<Item> SUN = REGISTRATE.item("sun", Item::new).lang("Sun").register();
 
 	public static void init() {
 		// load the class and register everything
