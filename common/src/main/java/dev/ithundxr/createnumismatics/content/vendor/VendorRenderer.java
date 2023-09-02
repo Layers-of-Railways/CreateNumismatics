e1717d9edad9d4e23039f17979f2cf0b23fe278d
IThundxr <contact@ithundxr.dev>
2023-09-02 10:57:15 -0400

    yipe

--- common/src/main/java/dev/ithundxr/createnumismatics/content/vendor/VendorRenderer.java
+++ common/src/main/java/dev/ithundxr/createnumismatics/content/vendor/VendorRenderer.java
@@ -3,20 +3,20 @@ package dev.ithundxr.createnumismatics.content.vendor;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import com.simibubi.create.foundation.utility.AnimationTickHolder;
-import com.simibubi.create.foundation.utility.Components;
-import dev.ithundxr.createnumismatics.Numismatics;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 public class VendorRenderer implements BlockEntityRenderer<VendorBlockEntity> {
+    public VendorRenderer(BlockEntityRendererProvider.Context context) {
+    }
+
     @Override
     public void render(@NotNull VendorBlockEntity blockEntity, float partialTick, @NotNull PoseStack poseStack,
                        @NotNull MultiBufferSource buffer, int packedLight, int packedOverlay) {
