9d8661133509e32bb71a193fc301952989b6db25
IThundxr <contact@ithundxr.dev>
2023-09-02 09:57:55 -0400

    mixin extras time lets go!!!

---
+++ common/src/main/java/dev/ithundxr/createnumismatics/mixin/MixinGoogleOverlayRenderer.java
@@ -0,0 +1,9 @@
+package dev.ithundxr.createnumismatics.mixin;
+
+import com.simibubi.create.content.equipment.goggles.GoggleOverlayRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GoggleOverlayRenderer.class)
+public class MixinGoogleOverlayRenderer {
+    // Hello Mixins :3
+}
