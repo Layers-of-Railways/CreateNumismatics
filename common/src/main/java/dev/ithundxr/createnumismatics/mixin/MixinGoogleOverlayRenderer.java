7e05d63360b9366aba0b488a07fbca7035cedc21
IThundxr <contact@ithundxr.dev>
2023-09-02 10:35:59 -0400

    feat: change the goggles overlay item to anything you want
    I didnt like the goggles :p

--- common/src/main/java/dev/ithundxr/createnumismatics/mixin/MixinGoogleOverlayRenderer.java
+++ common/src/main/java/dev/ithundxr/createnumismatics/mixin/MixinGoogleOverlayRenderer.java
@@ -1,9 +1,22 @@
 package dev.ithundxr.createnumismatics.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.equipment.goggles.GoggleOverlayRenderer;
+import com.simibubi.create.content.equipment.goggles.GogglesItem;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import dev.ithundxr.createnumismatics.util.ClientUtils;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GoggleOverlayRenderer.class)
 public class MixinGoogleOverlayRenderer {
-    // Hello Mixins :3
+    @WrapOperation(
+            method = "renderOverlay",
+            at = @At(value = "INVOKE", target = "Lcom/tterrag/registrate/util/entry/ItemEntry;asStack()Lnet/minecraft/world/item/ItemStack;")
+    )
+    private static ItemStack changeDisplayItem(ItemEntry<GogglesItem> instance, Operation<ItemStack> original) {
+        return ClientUtils.changeGoggleOverlayItem();
+    }
 }
--- common/src/main/java/dev/ithundxr/createnumismatics/util/ClientUtils.java
+++ common/src/main/java/dev/ithundxr/createnumismatics/util/ClientUtils.java
@@ -1,11 +1,17 @@
 package dev.ithundxr.createnumismatics.util;
 
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.equipment.goggles.GogglesItem;
+import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.ithundxr.createnumismatics.base.block.ForcedGoggleOverlay;
+import dev.ithundxr.createnumismatics.content.vendor.VendorBlockEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 
@@ -27,5 +33,24 @@ public class ClientUtils {
             return false;
 
         return level.getBlockState(blockHitResult.getBlockPos()).getBlock() instanceof ForcedGoggleOverlay;
+    }
+
+    public static ItemStack changeGoggleOverlayItem() {
+        ItemStack goggles = AllItems.GOGGLES.asStack();
+
+        HitResult hitResult = Minecraft.getInstance().hitResult;
+        if (!(hitResult instanceof BlockHitResult blockHitResult))
+            return goggles;
+
+        ClientLevel level = Minecraft.getInstance().level;
+        if (level == null)
+            return goggles;
+
+        if (level.getBlockEntity(blockHitResult.getBlockPos()) instanceof VendorBlockEntity vendorBlockEntity) {
+            // get the block entities cost and show the item for that and its cost and under
+            // show what is being sold (the enchants)
+            return Items.DIAMOND.getDefaultInstance();
+        }
+        return goggles;
     }
 }
