397ce00f327f744dec33323b7b1fb41e090bfd5a
IThundxr <me@ithundxr.dev>
2024-04-08 13:33:24 -0400

    v1.0.1

--- fabric/src/main/java/dev/ithundxr/createnumismatics/fabric/mixin/client/MixinGoggleOverlayRenderer.java
+++ fabric/src/main/java/dev/ithundxr/createnumismatics/fabric/mixin/client/MixinGoggleOverlayRenderer.java
@@ -1,4 +1,4 @@
-package dev.ithundxr.createnumismatics.mixin.client;
+package dev.ithundxr.createnumismatics.fabric.mixin.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
