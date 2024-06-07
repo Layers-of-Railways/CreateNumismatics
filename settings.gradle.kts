db21eb35b9b1a93e2b33201736c78c4032e68c63
IThundxr <me@ithundxr.dev>
2024-06-07 19:16:12 -0400

    stonecutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,50 @@
+/*
+ * Numismatics
+ * Copyright (c) 2024 The Railways Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import dev.kikugie.stonecutter.StonecutterSettings
+
 pluginManagement {
     repositories {
         maven("https://maven.fabricmc.net/")
         maven("https://maven.architectury.dev/")
         maven("https://maven.minecraftforge.net/")
         maven("https://maven.quiltmc.org/repository/release")
+        //maven("https://maven.kikugie.dev/releases")
+        maven("https://maven.kikugie.dev/snapshots")
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.4-beta.3" // https://github.com/kikugie/stonecutter-kt
+}
+
+extensions.configure<StonecutterSettings> {
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+
+    shared {
+        //fixme don't hardcode mc ver
+        vers("fabric", "1.20.1")
+        vers("forge", "1.20.1")
+    }
+
+    create(rootProject)
 }
 
 include("common")
