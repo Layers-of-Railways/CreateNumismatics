243e54371304b9337ce729775470fa3a48e9e4f0
IThundxr <me@ithundxr.dev>
2025-03-03 11:47:42 -0500

    Fix version range again

--- build.gradle.kts
+++ build.gradle.kts
@@ -172,7 +172,7 @@ subprojects {
             "fabric_loader_version" to "fabric_loader_version"(),
             "forge_version" to "forge_version"().split(".")[0], // only specify major version of forge
             "create_forge_version" to createForgeVersion, 
-            "create_forge_upper_bounds" to createForgeUpperBounds,
+            "create_forge_upper_bounds" to createForgeUpperBounds.invoke(),
             "create_fabric_version" to "create_fabric_version"().split("+")[0], // Trim +mcX.XX.X from version string
             "create_fabric_version_range" to "create_fabric_version_range"()
         )
