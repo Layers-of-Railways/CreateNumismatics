b4cbb96f5cd2fbec80fdbb2f6f3cc3abcd4f6bf5
techno-sam <77073745+techno-sam@users.noreply.github.com>
2026-08-27 11:32:41 +0200

    post-merge fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -273,6 +273,8 @@ subprojects {
             projectId = "curseforge_id"()
             accessToken = System.getenv("CURSEFORGE_TOKEN")
             minecraftVersions.add("minecraft_version"())
+            client.set(true)
+            server.set(true)
 
             requires {
                 slug = createVersionType
@@ -283,6 +285,7 @@ subprojects {
             projectId = "modrinth_id"()
             accessToken = System.getenv("MODRINTH_TOKEN")
             minecraftVersions.add("minecraft_version"())
+            environment.set(CLIENT_AND_SERVER)
 
             requires {
                 slug = createVersionType
